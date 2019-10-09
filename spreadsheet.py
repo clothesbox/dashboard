2bedb0b055421c48aaf3a0c5fda99b7cf67284d7
aman-am <amanmehta015@gmail.com>
2019-10-09 16:10:20 +0530

    Google Spreadsheets linking

---
+++ spreadsheet.py
@@ -0,0 +1,20 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("Create Pickup").sheet1
+
+# Extract and print all of the values
+orderIDs = sheet.col_values(7)
+emailIDs = sheet.col_values(14)
+
+print(emailIDs)
